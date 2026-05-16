82766d12119887bd563323dc8583f02baaf66add
rlerenoi <rachidiel61@gmail.com>
2026-05-16 23:54:42 +0100

    add new modif

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -1,10 +1,18 @@
+import { IconBrandFacebook, IconBrandInstagram, IconBrandTiktok, IconBrandTwitter } from "@tabler/icons-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-beauty-cream text-white py-5 text-center px-5 border-t">
-      <p className="mt-4 text-gray-500">
-        © 2026 — Tous droits réservés.
-      </p>
+    <footer className="block md:flex md:justify-between bg-beauty-rose text-white py-2 text-center px-20 border-t">
+      <div className="flex items-center gap-4 text-beauty-cream justify-center">
+        <IconBrandFacebook stroke={2} />
+        <IconBrandInstagram stroke={2} />
+        <IconBrandTwitter stroke={2} />
+        <IconBrandTiktok stroke={2} />
+      </div>
+
+      <div className="mt-4 text-beauty-cream flex items-center justify-center">
+        © {new Date().getFullYear()} - Joyce Beauy. Tous droits réservés.
+      </div>
     </footer>
   )
 }
